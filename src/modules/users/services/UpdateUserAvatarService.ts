29b2b1c141fcade34409714f14b84b68ae6f0b24
felipelhol <felipelhol@gmail.com>
2020-08-26 18:34:53 -0300

    "new jtests and inversion dependencies"

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -5,6 +5,7 @@ import uploadConfig from '@config/upload';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
 import User from '../infra/typeorm/entities/User';
@@ -19,6 +20,9 @@ class UpdateUserAvataService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -30,16 +34,19 @@ class UpdateUserAvataService {
       throw new AppError('Only authenticated users can change avatar', 401);
     }
     if (user.avatar) {
+      await this.storageProvider.deleteFile(user.avatar);
       // deletar avatar anterior
 
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+      //  const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      //  const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
 
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      //  if (userAvatarFileExists) {
+      //    await fs.promises.unlink(userAvatarFilePath);
+      //  }
     }
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
